cc3d35e798d25f376c4c7ae80f0c1e5e64846793
ofir6bd <ofir6bd@gmail.com>
2025-01-11 18:46:18 +0200

    almost done

--- HM3_DataStructure/src/Control/Main.java
+++ HM3_DataStructure/src/Control/Main.java
@@ -9,7 +9,7 @@ public class Main {
         // Get the singleton instance of ProdManLogic
         ProdManLogic prodMan = ProdManLogic.getInstance();
         
-        // Add products to the inventory
+        // Add 10 products to the inventory
         prodMan.addProduct(1, "Table", 64);
         prodMan.addProduct(2, "Chair", 40);
         prodMan.addProduct(3, "Picture", 20);
@@ -21,7 +21,56 @@ public class Main {
         prodMan.addProduct(9, "Dresser", 22);
         prodMan.addProduct(10, "Nightstand", 74);
         
+        ////// Orders //////
+        // Get the singleton instance of OrdManLogic
+        OrdManLogic ordMan = OrdManLogic.getInstance();
+        
+        // Add 10 orders
+        ordMan.addOrder("Israel, Tel Aviv, Rothschild Blvd 1", 1, new int[]{1, 2, 3, 4, 5, 6, 7, 8});
+        ordMan.addOrder("Israel, Rishon LeZion, Rabin St 2", 2, new int[]{1, 2, 3, 4, 5, 6, 7, 80}); 
+        ordMan.addOrder("Israel, Haifa, Herzl St 3", 3, new int[]{1, 2, 3, 4, 5, 6});
+        ordMan.addOrder("Israel, Eilat, Ben Gurion St 4", 4, new int[]{1, 2, 3, 4, 66, 5, 6, 7, 8, 3}); 
+        ordMan.addOrder("Israel, Jerusalem, King George St 5", 5, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        ordMan.addOrder("Israel, Beersheba, Weizmann St 6", 1, new int[]{1, 2, 3, 4, 5, 6, 7, 100}); 
+        ordMan.addOrder("Israel, Netanya, Dizengoff St 7", 1, new int[]{2, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
+        ordMan.addOrder("Israel, Ashdod, Begin St 8", 2, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 1}); 
+        ordMan.addOrder("Israel, Holon, Jabotinsky St 9", 3, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 2, 14});
+        ordMan.addOrder("Israel, Bat-Yam, Jabotinsky St 10", 1, new int[]{1, 2, 2, 4, 3, 6, 4, 8, 5, 10, 6, 12, 7, 2});
+        
+        // Insert 5 more products
+        prodMan.addProduct(11, "Wardrobe", 30);
+        prodMan.addProduct(12, "Stool", 30);
+        prodMan.addProduct(13, "Mirror", 30);
+        prodMan.addProduct(14, "TV Stand", 1);
+        prodMan.addProduct(15, "Ottoman", 1);
+        
+        System.out.println("\n----------------------------------------------------------------------------------------------");
+        System.out.println("--------------------Process the top 3 prioritized orders (per capability)---------------------");
         System.out.println("----------------------------------------------------------------------------------------------");
+        // Process the next 3 orders
+        ordMan.processNextOrder(3);
+        
+        System.out.println("\n----------------------------------------------------------------------------------------------");
+        System.out.println("---------------------------------------------Reports------------------------------------------");
+        System.out.println("----------------------------------------------------------------------------------------------");
+        System.out.println("");
+        
+        // Get the instance of Reports
+        Reports repMan = new Reports();
+        
+	    // Generate and print the total orders report
+	    repMan.totalOrdersReport();
+	    
+        // Generate and print the inventory report
+        repMan.getInventoryReport();
+        
+        // Generate and print the report of the k biggest orders
+        repMan.getKBiggestOrders(3);
+        
+        
+        
+        
+        System.out.println("\n\n\n\n----------------------------------------------------------------------------------------------");
         System.out.println("---------------------------------Testing Actions on products----------------------------------");
         System.out.println("----------------------------------------------------------------------------------------------");
         // Check availability of products
@@ -55,22 +104,6 @@ public class Main {
         System.out.println("Reduce Quantity(More than available): " + prodMan.reduceQuantity(productsToCheck)); 
         System.out.println("Delete Product(ProductID not exist): " + prodMan.deleteProduct(99)); 
  
-        ////// Orders //////
-        // Get the singleton instance of OrdManLogic
-        OrdManLogic ordMan = OrdManLogic.getInstance();
-        
-        // Add orders
-        ordMan.addOrder("Israel, Tel Aviv, Rothschild Blvd 1", 1, new int[]{1, 2, 3, 4, 5, 6, 7, 8});
-        ordMan.addOrder("Israel, Rishon LeZion, Rabin St 2", 2, new int[]{1, 2, 3, 4, 5, 6, 7, 80}); 
-        ordMan.addOrder("Israel, Haifa, Herzl St 3", 3, new int[]{1, 2, 3, 4, 5, 6});
-        ordMan.addOrder("Israel, Eilat, Ben Gurion St 4", 4, new int[]{1, 2, 3, 4, 66, 5, 6, 7, 8, 3}); 
-        ordMan.addOrder("Israel, Jerusalem, King George St 5", 5, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
-        ordMan.addOrder("Israel, Beersheba, Weizmann St 6", 1, new int[]{1, 2, 3, 4, 5, 6, 7, 100}); 
-        ordMan.addOrder("Israel, Netanya, Dizengoff St 7", 1, new int[]{2, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
-        ordMan.addOrder("Israel, Ashdod, Begin St 8", 2, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 1}); 
-        ordMan.addOrder("Israel, Holon, Jabotinsky St 9", 3, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 2, 14});
-        ordMan.addOrder("Israel, Bat-Yam, Jabotinsky St 10", 1, new int[]{1, 2, 2, 4, 3, 6, 4, 8, 5, 10, 6, 12, 7, 2});
-        
         System.out.println("\n----------------------------------------------------------------------------------------------");
         System.out.println("------------------Testing edge cases on orders - all should return 'false'------------------");
         System.out.println("----------------------------------------------------------------------------------------------");
@@ -78,34 +111,5 @@ public class Main {
         System.out.println("Add Order (Priority not between 1-5): " + ordMan.addOrder("", 1, new int[]{1, 2, 3, 4, 5, 6, 7, 8}));
         System.out.println("Add Order (Odd numbers- meanining no quantity for last product): " + ordMan.addOrder("Israel, Jerusalem, Ramot 4", 1, new int[]{1, 7, 3}));
         
-        // Insert 5 more products
-        prodMan.addProduct(11, "Wardrobe", 30);
-        prodMan.addProduct(12, "Stool", 30);
-        prodMan.addProduct(13, "Mirror", 30);
-        prodMan.addProduct(14, "TV Stand", 1);
-        prodMan.addProduct(15, "Ottoman", 1);
-        
-        System.out.println("\n----------------------------------------------------------------------------------------------");
-        System.out.println("--------------------Process the top 3 prioritized orders (per capability)---------------------");
-        System.out.println("----------------------------------------------------------------------------------------------");
-        // Process the next 3 orders
-        ordMan.processNextOrder(3);
-        
-        System.out.println("\n----------------------------------------------------------------------------------------------");
-        System.out.println("---------------------------------------------Reports------------------------------------------");
-        System.out.println("----------------------------------------------------------------------------------------------");
-        System.out.println("");
-        
-        // Get the instance of Reports
-        Reports repMan = new Reports();
-        
-	    // Generate and print the total orders report
-	    repMan.totalOrdersReport();
-	    
-        // Generate and print the inventory report
-        repMan.getInventoryReport();
-        
-        // Generate and print the report of the k biggest orders
-        repMan.getKBiggestOrders(3);
     }
 }
