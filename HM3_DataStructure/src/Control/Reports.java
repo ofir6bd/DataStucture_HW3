067f8ab9b9532a1fc543690914fbd4b7e877fec5
ofir6bd <ofir6bd@gmail.com>
2025-01-12 10:49:02 +0200

    all done

--- HM3_DataStructure/src/Control/Reports.java
+++ HM3_DataStructure/src/Control/Reports.java
@@ -109,32 +109,25 @@ class AVLTree {
 
         // Update height of this ancestor node 
         node.height = 1 + Math.max(height(node.left), height(node.right)); 
-
         // Get the balance factor of this ancestor node 
         int balance = getBalance(node); 
-
         // If this node becomes unbalanced, then there are 4 cases
-
         // Left Left Case 
         if (balance > 1 && key < node.left.key) 
             return rightRotate(node); 
-
         // Right Right Case 
         if (balance < -1 && key > node.right.key) 
             return leftRotate(node); 
-
         // Left Right Case 
         if (balance > 1 && key > node.left.key) { 
             node.left = leftRotate(node.left); 
             return rightRotate(node); 
         } 
-
         // Right Left Case 
         if (balance < -1 && key < node.right.key) { 
             node.right = rightRotate(node.right); 
             return leftRotate(node); 
         } 
-
         // Return the (unchanged) node pointer 
         return node; 
     }
